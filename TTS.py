b8411af6d529c858586de7dd3838d381c8442a16
SATISH <satishofcit@gmail.com>
2019-05-27 21:05:04 +0530

    STT module code updated

--- TTS.py
+++ TTS.py
@@ -1,1 +1,7 @@
 #This module conver text to speech
+from gtts import gTTS
+
+customer = 'Hafidz'
+amount = 20000
+tts = gTTS('hello' + customer + 'have you applied for loan of amount' + str(amount), lang='en')
+tts.save('hello.mp3')
